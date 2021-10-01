b174ba9151120ad92a9e4d5481e3683d8bc04ded
Ljubica7 <bubedoneva7@gmail.com>
2021-10-01 11:51:56 +0200

    auth

--- src/index.js
+++ src/index.js
@@ -6,16 +6,27 @@ import { ProductsProvider } from './context/products_context';
 import { FilterProvider } from './context/filter_context';
 import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
-// import {Auth0Provider} from '@auth0/auth0-react'
+import { Auth0Provider } from '@auth0/auth0-react';
+// dev-8-rfn8pb.us.auth0.com   - domain
+// SN9tpbDhjtT9quALcCDmw4WpJg6ltXBG   - client id
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain='dev-8-rfn8pb.us.auth0.com'
+    clientId='SN9tpbDhjtT9quALcCDmw4WpJg6ltXBG'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
 
   document.getElementById('root')
 );
